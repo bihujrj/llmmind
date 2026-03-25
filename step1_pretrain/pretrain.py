674c9e1fc7ea117abd6e4eaa5331a37bc55d2602
18811392054 <57883447@qq.com>
2026-03-25 16:51:34 +0800

    t

--- step1_pretrain/pretrain.py
+++ step1_pretrain/pretrain.py
@@ -31,7 +31,7 @@ def train_epoch(epoch, model, loader, optimizer, scaler, args, wandb=None, start
     model.train()
     total_steps = len(loader) + start_step
     start_time = time.time()
-
+    print(start_time)
     for step, (input_ids, labels) in enumerate(loader, start=start_step + 1):
         # 将数据移到 GPU（如果可用）
         input_ids = input_ids.to(args.device, non_blocking=True)
@@ -112,6 +112,9 @@ def train_epoch(epoch, model, loader, optimizer, scaler, args, wandb=None, start
                     wandb=wandb
                 )
                 del state_dict
+    Finish_time = time.time()
+    print(start_time)
+    print("Finish_time-start_time: "+(Finish_time-start_time))
 
 def main():
     parser = argparse.ArgumentParser(description="Llmmind Pretraining (GPU Optimized)")
